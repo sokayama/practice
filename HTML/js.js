bdad134baebbe9a62c1b346b8dd5aaede45def7f
Okayama Shuhei <shuhei_o@digirea.com>
2016-07-05 17:50:38 +0900

    add "createElement()"

--- HTML/js.js
+++ HTML/js.js
@@ -5,3 +5,8 @@ ele.textContent = "zxcv";
 
 var parent = ele.parentNode;
 parent.removeChild(ele);
+
+
+var newElement = document.createElement("h1");
+document.body.appendChild(newElement);
+newElement.textContent = "createelement";
