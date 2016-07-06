85803693b77a84aca3be36f1e2211e4e096782f2
Okayama Shuhei <shuhei_o@digirea.com>
2016-07-06 11:01:16 +0900

    add  "requestAnimationFrame()"

--- HTML2/js.js
+++ HTML2/js.js
@@ -1,9 +1,20 @@
 
-var idButton = document.getElementById("button");
+window.addEventListener("load",main,false);
 
-idButton.addEventListener("click",function(){
-	setTimeout(function(){
-		alert("clicked!!!")
-	},3000)
-} ,false);
+function main()
+{
+	var idEle = document.getElementById("idEle");
 
+	var top = 10;
+
+	function timerFunc()
+	{
+		top++;
+
+		idEle.style.top = top + "px";
+		idEle.style.left = "30px";
+		requestAnimationFrame(timerFunc);
+	}
+
+	timerFunc();
+}
